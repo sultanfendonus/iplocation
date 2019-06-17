f076bce6a82e1c8fd6f099d607cec3f95b0364d5
Sunny Sultan <sunnysultan1640@gmail.com>
2019-06-17 13:23:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+var ip2loc = require("ip2location-nodejs");
+const App = express();
+// Create application/json parser
+App.use(bodyParser());
+App.use(bodyParser.urlencoded({extended: true}));
+
+//Get Ip adress
+
+App.get('/getIP', getClientIP);
+
+function getClientIP(req, res, next) {
+  var ip = req.ip;
+  //var ip = '::ffff:103.229.83.102';
+  var realIp = ip.split(":").reverse()[0];
+
+  ip2loc.IP2Location_init('./src/IP2LOCATION-LITE-DB9.BIN');
+  result = ip2loc.IP2Location_get_all(realIp);
+  res.send({ip : result.ip, country_short : result.country_short,country_long : result.country_long,
+    region : result.region, city : result.city, latitude : result.latitude, longitude : result.longitude , zipcode : result.zipcode,
+    status : result.status});
+  
+}
+
+
+App.listen("3001",()=>console.log("Connected !!"));
